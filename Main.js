696cdb89a16be2888fbffc0445fd179b746659a5
Mohamed <mohamedabuali97@gmail.com>
2025-11-23 23:55:01 -0600

    feat: Add basic body styling and render a new example UI component.

--- Main.js
+++ Main.js
@@ -13,7 +13,7 @@ element("div", null, "Hello!")
 element("button", { onClick: handleClick }, "Click Me")
 element("ul", null, items.map((item, i) => hs("li", { id: i }, item)))
 
-
+element("div", null, hs('h1', null, 'Hello World'), hs('p', null, 'This is a paragraph'), hs('button', { onClick: handleClick }, 'Click Me'))
 
 
 
